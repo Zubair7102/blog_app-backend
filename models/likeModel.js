9c033fdbe115cb74cf8f8a42c42cd28d488ce288
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-08-09 01:33:05 +0530

    added code and logic

--- models/likeModel.js
+++ models/likeModel.js
@@ -3,23 +3,22 @@ const mongoose = require("mongoose");
 
 // route Handler
 const likeSchema = new mongoose.Schema({
-    post:{
-        type: mongoose.Schema.Types.ObjectId,
-        // n MongoDB, every document (like a post or comment) has a unique identifier called an ObjectId. This ID is a special string of characters that MongoDB generates to uniquely identify each document.
-        // When you define a field in a Mongoose schema with type: mongoose.Schema.Types.ObjectId, you're telling Mongoose that this field will store one of these unique ObjectIds.
-        ref: "Post",
-        // ref: "Post": This means that the ObjectId stored in this field is an ID from the Post model 
-        // The ref option tells Mongoose which model this ObjectId is referencing.
-    },
-    user:{
-        type: String,
-        required: true,
-    },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    // n MongoDB, every document (like a post or comment) has a unique identifier called an ObjectId. This ID is a special string of characters that MongoDB generates to uniquely identify each document.
+    // When you define a field in a Mongoose schema with type: mongoose.Schema.Types.ObjectId, you're telling Mongoose that this field will store one of these unique ObjectIds.
+    ref: "Post",
+    // ref: "Post": This means that the ObjectId stored in this field is an ID from the Post model
+    // The ref option tells Mongoose which model this ObjectId is referencing.
+  },
+  user: {
+    type: String,
+    required: true,
+  },
 });
 
-// export 
+// export
 module.exports = mongoose.model("Like", likeSchema);
-
 
 // mongoose.model():
 // This is a Mongoose method that creates a model.
@@ -27,12 +26,9 @@ module.exports = mongoose.model("Like", likeSchema);
 // The name of the model (in this case, "Like").
 // The schema that defines the structure of the documents in this model (in this case, likeSchema).
 
-
 // "Like":
 // This is the name of your model.
-// In MongoDB, Mongoose will automatically create a collection with the pluralized version of this name, so "Like" becomes the "likes" collection in your database. 
-
-
+// In MongoDB, Mongoose will automatically create a collection with the pluralized version of this name, so "Like" becomes the "likes" collection in your database.
 
 // likeSchema:
 // This is the schema that you’ve defined earlier in your code, which specifies the structure of the documents in the likes collection.
--- routes/blog.js
+++ routes/blog.js
@@ -3,4 +3,13 @@ const router = express.Router();
 // express.Router() is a function in Express.js that creates a new "mini" router or a modular, mountable route handler. Think of it like a mini version of the main Express app, designed to handle a specific group of routes.
 
 // IMPORT THE CONTROLLER
-const {}
+const {dummyLink} = require("../controllers/likeController");
+
+
+// Mapping Create
+router.get("/dummyroute", dummyLink);
+
+
+
+// export
+module.exports = router;
