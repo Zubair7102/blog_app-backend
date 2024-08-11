d2ad57e5c848153d82692ebc3202cc1ae6c5f3f3
Mohd Zubair Ahmed <jubaira646@gmail.com>
2024-08-12 00:44:34 +0530

    like & unlike cntrl

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -13,8 +13,8 @@ exports.likePost = async (req, res) => {
         const savedLike = await like.save();
 
         // update the post collection basis on this
-        const updatedPost = await Post.findByIdAndUpdate(post, {$push: {likes: savedLike._id}}, {new: true});
-        
+        const updatedPost = await Post.findByIdAndUpdate(post, {$push: {likes: savedLike._id}}, {new: true})
+        .populate("likes").exec();
 
         res.status(200).json({
             post: updatedPost,
@@ -25,7 +25,34 @@ exports.likePost = async (req, res) => {
     catch(error)
     {
         return res.status(500).json({
-            error: "Error while fetching post"
+            error: "Error while Liking post"
         });
     }
+}
+
+// Unlike a post 
+exports.unlikePost = async(req, res) =>{
+    try{
+        const{post, like} = req.body;
+        // find and delete the like from the collection
+        const deletedLike = await Like.findOneAndDelete({post: post, _id:like});
+        // findOneAndDelete():This is a Mongoose method that finds the first document matching the query criteria and deletes it from the collection.
+        // post: post: This part of the query looks for a Like document that is associated with the specified post. post is a variable representing the ID of the post in question.
+        // _id: like: This part of the query looks for a Like document with the specified _id, where like is a variable representing the ID of the like you want to delete.
+        // deletedLike: This variable will hold the deleted Like document, including its _id, which is important for the next step.
+        
+        // update the post collection 
+        const updatedPost = await Post.findByIdAndDelete(post, {$pull: {likes: deletedLike._id}})
+        // post: This is the ID of the post you want to update, which you pass to findByIdAndUpdate() to locate the specific post document.
+        // $pull: This is a MongoDB operator that removes elements from an array that match a specified condition.
+        // { likes: deletedLike._id }: This tells MongoDB to remove the deletedLike._id from the likes array in the Post document. The likes array stores the IDs of all the likes associated with that post.
+
+
+    }
+    catch(error)
+        {
+            return res.status(500).json({
+                error: "Error while Unliking post"
+            });
+        }
 }
--- routes/blog.js
+++ routes/blog.js
@@ -3,7 +3,7 @@ const router = express.Router();
 // express.Router() is a function in Express.js that creates a new "mini" router or a modular, mountable route handler. Think of it like a mini version of the main Express app, designed to handle a specific group of routes.
 
 // IMPORT THE CONTROLLER
-const {likePost} = require("../controllers/likeController")
+const {likePost} = require("../controllers/LikeController")
 const {createComment} = require("../controllers/CommentController");
 const {createPost, getAllPosts} = require("../controllers/PostController");
 
